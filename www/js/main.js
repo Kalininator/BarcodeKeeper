10ebd9a9566044ee4ee5c9ee00adf78661476f64
Alexander Kalinin <alex@kalinin.uk>
2018-05-02 06:54:52 +0100

    new clicky mcfuck thing

--- www/js/main.js
+++ www/js/main.js
@@ -250,20 +250,11 @@ function showEditView(name,success,fail){
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -33.8688, lng: 151.2195},
-        zoom: 13
+        zoom: 17
     });
-    var card = document.getElementById('pac-card');
     var input = document.getElementById('pac-input');
-
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
-
     var autocomplete = new google.maps.places.Autocomplete(input);
-
-    // Bind the map's bounds (viewport) property to the autocomplete object,
-    // so that the autocomplete requests use the current map bounds for the
-    // bounds option in the request.
     autocomplete.bindTo('bounds', map);
-
     var infowindow = new google.maps.InfoWindow();
     var infowindowContent = document.getElementById('infowindow-content');
     infowindow.setContent(infowindowContent);
@@ -271,29 +262,23 @@ function initMap() {
         map: map,
         anchorPoint: new google.maps.Point(0, -29)
     });
-
     autocomplete.addListener('place_changed', function() {
         infowindow.close();
         marker.setVisible(false);
         var place = autocomplete.getPlace();
         if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
             window.alert("No details available for input: '" + place.name + "'");
             return;
         }
-
-        // If the place has a geometry, then present it on a map.
         if (place.geometry.viewport) {
             map.fitBounds(place.geometry.viewport);
         } else {
             map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
+            map.setZoom(17);
         }
-        alert(place.geometry.location);
+        alert(place.geometry.location + "," + place.name);
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
-
         var address = '';
         if (place.address_components) {
             address = [
@@ -302,27 +287,32 @@ function initMap() {
                 (place.address_components[2] && place.address_components[2].short_name || '')
             ].join(' ');
         }
-
         infowindowContent.children['place-icon'].src = place.icon;
         infowindowContent.children['place-name'].textContent = place.name;
         infowindowContent.children['place-address'].textContent = address;
         infowindow.open(map, marker);
     });
+    placesService = new google.maps.places.PlacesService(map);
+    map.addListener('click',function(event){
+        alert(event.latLng);
+        if(event.placeId){
+            event.stop();
+            placesService.getDetails({placeId: placeId}, function(place, status) {
+                if (status === 'OK') {
+                    // me.infowindow.close();
+                    // me.infowindow.setPosition(place.geometry.location);
+                    // me.infowindowContent.children['place-icon'].src = place.icon;
+                    // me.infowindowContent.children['place-name'].textContent = place.name;
+                    // me.infowindowContent.children['place-id'].textContent = place.place_id;
+                    // me.infowindowContent.children['place-address'].textContent =
+                    //     place.formatted_address;
+                    // me.infowindow.open(me.map);
+                    alert(place.name);
+                }
+            });
 
-    // Sets a listener on a radio button to change the filter type on Places
-    // Autocomplete.
-    function setupClickListener(id, types) {
-        var radioButton = document.getElementById(id);
-        radioButton.addEventListener('click', function() {
-            autocomplete.setTypes(types);
-        });
-    }
-
-    setupClickListener('changetype-all', []);
-    setupClickListener('changetype-address', ['address']);
-    setupClickListener('changetype-establishment', ['establishment']);
-    setupClickListener('changetype-geocode', ['geocode']);
-
+        }
+    });
 }
 
 
